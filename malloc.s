d6f847c1896aca1a3faae58971a1eaec551581f7
Thilons <tgcl21@inf.ufpr.br>
2023-11-16 17:13:38 -0300

    Set dirty bit

--- malloc.s
+++ malloc.s
@@ -1,27 +1,27 @@
 .section .data
     I: .quad 0
-    topoInicialHeap: .quad 0
-    inicioHeapLk: .quad 0
-    topoHeapLk: .quad 0
+    INITIAL_TOP_HEAP: .quad 0
+    INITIAL_LK: .quad 0
+    TOP_LK: .quad 0
     str: .string "TESTE"
 .section .text
 .global _start
 
 iniciaAlocador:
-    # Requests the top of the heap, (brk), saves in the global variable topoInicialHeap
+    # Requests the top of the heap, (brk), saves in the global variable INITIAL_TOP_HEAP
     pushq %rbp
     movq %rsp, %rbp                     # Activate the stack frame
 
-    cmpq $0, topoInicialHeap            # IF brk was not set
+    cmpq $0, INITIAL_TOP_HEAP            # IF brk was not set
     jne fim_inicia                      # THEN goto finalizaAlocador;
 
     # get the top of the heap
     movq $0, %rdi                       # %rdi = 0;
     movq $12, %rax                      # %rax = 12;
     syscall 
-    movq %rax, topoInicialHeap          # topoInicialHeap = %rax;
-    movq %rax, topoHeapLk               # topoHeapLk = topoInicialHeap;
-    movq %rax, inicioHeapLk             # inicioHeapLk = topoInicialHeap;
+    movq %rax, INITIAL_TOP_HEAP          # INITIAL_TOP_HEAP = %rax;
+    movq %rax, TOP_LK               # TOP_LK = INITIAL_TOP_HEAP;
+    movq %rax, INITIAL_LK             # INITIAL_LK = INITIAL_TOP_HEAP;
 
     fim_inicia:
     pop %rbp
@@ -32,7 +32,7 @@ finalizaAlocador:
     pushq %rbp
     movq %rsp, %rbp
 
-    movq topoInicialHeap, %rdi
+    movq INITIAL_TOP_HEAP, %rdi
     movq $12, %rax
     syscall
     
@@ -52,12 +52,14 @@ criaBloco:
     movq %rsp, %rbp
 
     movq %rdi, %rbx                 # Get the size of the block to be created from the stack
-    movq topoHeapLk, %rax               # Get the top of the heap
+    movq %rdi, %rcx                 # Get the size of the block to be created from the stack
+
+    movq TOP_LK, %rax               # Get the top of the heap
     addq %rbx, %rax                     # Add the size of the block to the top of the heap
     addq $16, %rax                      # Add 16 bytes to the top of the heap for the dirty bit and the size of the block
 
-    movq %rax, topoHeapLk               # Set the new top of the heap
-    movq topoHeapLk, %rdi               # Set the new top of the heap as the argument for the brk syscall
+    movq %rax, TOP_LK               # Set the new top of the heap
+    movq TOP_LK, %rdi               # Set the new top of the heap as the argument for the brk syscall
     movq $12, %rax                      # Set the brk syscall number
     syscall                             # Call the brk syscall
 
@@ -66,16 +68,19 @@ criaBloco:
     je fim_criaBloco                   # THEN goto fim_criaBloco; 
 
     # system call succeeded
-    movq topoHeapLk, %rax               # Get the top of the heap
+    movq TOP_LK, %rax               # Get the top of the heap
     # sub size of the block from the top of the heap
     subq %rbx, %rax                     # Subtract the size of the block from the top of the heap
     subq $16, %rax                      # Subtract 16 bytes from the top of the heap for the dirty bit and the size of the block
-    # Set the dirty bit
-    movq $1, %rbx                       # Set the dirty bit to 1
-    movq %rbx, (%rax)                   # Set the dirty bit in the first 8 bytes of the block
-    # Set the size of the block
-    movq %rdi, %rbx                 # Get the size of the block to be created from the stack
-    movq %rbx, 8(%rax)                  # Set the size of the block in the second 8 bytes of the block
+
+    # Goes to the first 8 bytes of the block and sets the dirty bit to 0
+    movq %rax, %rbx                     # Get the address of the dirty bit
+    movq $1, 8(%rbx)                    # Set the dirty bit to 0
+    movq $rcx, 16(%rbx)                 # Set the size of the block
+
+    
+    # TODO save in linked list
+
 
     fim_criaBloco:
     pop %rbp
@@ -93,6 +98,6 @@ _start:
     
 
     movq $60, %rax
-    movq topoHeapLk, %rdi
+    movq TOP_LK, %rdi
     syscall
 
